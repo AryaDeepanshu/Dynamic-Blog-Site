3b38c3406019772b14e4ded0def273aed387f8dd
Deepanshu Arya <deepanshu.da85@gmail.com>
2021-09-20 23:38:15 +0530

    edit post working

--- admin/editpost.php
+++ admin/editpost.php
@@ -38,91 +38,91 @@ if(isset($_SESSION['author_role'])){
                 </div>
                 <div>
                     <h1>Edit Post:</h1>
-                    
+                    <?php
+                        $post_id = $_GET['id'];
+                        $FormSql = "SELECT * FROM `post` WHERE post_id='$post_id'";
+                        $FormResult = mysqli_query($conn, $FormSql);
+                        while($FormRow = mysqli_fetch_assoc($FormResult)){
+                            ?>
                     <form method="POST" enctype="multipart/form-data">
                         <div class="mb-3">
                             <label for="exampleInputTitile" class="form-label">Title</label>
-                            <input name="post_title" type="text" class="form-control" id="exampleInputTitile" aria-describedby="emailHelp" placeholder="Post Title">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputTitile" class="form-label">Category</label>
-                            <select name="post_category" class="form-select" aria-label="Default select example" placeholder="Select Category">
-                                <?php
-                                    $sql = "SELECT * FROM `category` ORDER BY category_id";
-                                    $result = mysqli_query($conn,$sql);
-                                    while($row = mysqli_fetch_assoc($result)){
-                                    echo '<option value="'.$row['category_id'].'">'.$row['category_name'].'</option>';
-                                    }
-                                ?>
-                            </select>
+                            <input name="post_title" type="text" class="form-control" id="exampleInputTitile" aria-describedby="emailHelp" value="<?php echo $FormRow['post_title']; ?>">
                         </div>
                         
                         <div class="mb-3">
                             <label for="exampleInputContent" class="form-label">Content</label>
-                            <textarea name="post_content" class="form-control" id="exampleInputTextArea1" aria-describedby="emailHelp" placeholder="Post Content"></textarea>
+                            <textarea name="post_content" class="form-control" id="exampleInputTextArea1" aria-describedby="emailHelp" rows="9"><?php echo $FormRow['post_content']; ?></textarea>
                         </div>
+
                         <div class="mb-3">
-                            <label for="exampleInputImage" class="form-label">Image</label>
-                            <input name="file" type="file" class="form-control" id="exampleInputImage" aria-describedby="emailHelp">
+                            <label for="exampleInputImage" class="form-label">Image</label><br>
+                            <img src="../<?php echo $FormRow['post_image'];?>"><br>
+                            <br><input name="file" type="file" class="form-control" id="exampleInputImage" aria-describedby="emailHelp">
                         </div>
-                        <div class="mb-3">
-                            <label for="exampleInputKeyword" class="form-label">Keywords</label>
-                            <input name="post_keyword" type="text" class="form-control" id="exampleInputKeyword" aria-describedby="emailHelp" placeholder="Post Keyword">
-                        </div>
-                        <button name="post" type="submit" class="btn btn-primary">Post</button>
+
+                        <button name="editpost" type="submit" class="btn btn-primary">Update</button>
                     </form>
+                    <?php } ?>
                     <?php
-                        if(isset($_POST['post'])){
+                        if(isset($_POST['editpost'])){
                             $post_title = mysqli_escape_string($conn, $_POST['post_title']);
                             $post_content = mysqli_escape_string($conn, $_POST['post_content']);
-                            $post_category = mysqli_escape_string($conn, $_POST['post_category']);
                             $post_keyword = mysqli_escape_string($conn, $_POST['post_keyword']);
-                            $post_author = $_SESSION['author_id'];
-                            $post_date = date("d/m/y");
-                            if(empty($post_title) OR empty($post_content) OR empty($post_category)){
-                                header("Location:newpost.php?message=Empty+fields");
+                            if(empty($post_title) OR empty($post_content)){
+                                echo("<script>location.href = '/admin/newpost.php?message=Empty+fields';</script>");
                                 exit();
                             }
-                
 
-                            $file = $_FILES['file'];
-                            $fileName = $file['name'];
-                            $fileType = $file['type'];
-                            $fileTmp = $file['tmp_name'];
-                            $fileErr = $file['error'];
-                            $fileSize = $file['size'];
-                            $fileEXT = explode('.', $fileName);
-                            $fileExtention = strtolower(end($fileEXT));
-                            $allowedEXT = array('jpg', 'jpeg', 'png');
-                            if(in_array($fileExtention,$allowedEXT)){
-                                if($fileErr === 0){
-                                    if($fileSize <300000){
-                                        $newFileName = uniqid('',true).'.'.$fileExtention;
-                                        $destination = "../uploads/$newFileName";
-                                        $dbdestination = "uploads/$newFileName";
-                                        move_uploaded_file($fileTmp, $destination);
-                                        echo 'Image Uploaded';
-                                        $sql = "INSERT INTO `post` (`post_title`, `post_content`, `post_image`, 
-                                        `post_date`, `post_category`, `post_author`, `post_keyword`) VALUES('$post_title', 
-                                        '$post_content', '$dbdestination', '$post_date', '$post_category', '$post_author', '$post_keyword')";
-                                        if(mysqli_query($conn,$sql)){
-                                            echo("<script>location.href = '/admin/post.php?msg=Post+Published';</script>");
-                                            exit();
+                            if(is_uploaded_file($_FILES['file']['tmp_name'])){
+                                $file = $_FILES['file'];
+                                $fileName = $file['name'];
+                                $fileType = $file['type'];
+                                $fileTmp = $file['tmp_name'];
+                                $fileErr = $file['error'];
+                                $fileSize = $file['size'];
+                                $fileEXT = explode('.', $fileName);
+                                $fileExtention = strtolower(end($fileEXT));
+                                $allowedEXT = array('jpg', 'jpeg', 'png');
+                                if(in_array($fileExtention,$allowedEXT)){
+                                    if($fileErr === 0){
+                                        if($fileSize <300000){
+                                            $newFileName = uniqid('',true).'.'.$fileExtention;
+                                            $destination = "../uploads/$newFileName";
+                                            $dbdestination = "uploads/$newFileName";
+                                            move_uploaded_file($fileTmp, $destination);
+                                            echo 'Image Uploaded';
+                                            $sql = "UPDATE `post` SET post_title='$post_title', post_content='$post_content', post_keyword='$post_keyword', post_image='$dbdestination' WHERE post_id='$post_id'";
+                                            if(mysqli_query($conn,$sql)){
+                                                echo("<script>location.href = '/admin/post.php?msg=Post+Updated';</script>");
+                                                exit();
+                                            }else{
+                                                echo("<script>location.href = '/admin/newpost.php?msg=error';</script>");
+                                                exit(); 
+                                            }  
                                         }else{
-                                            echo("<script>location.href = '/admin/newpost.php?msg=error';</script>");
-                                            exit(); 
-                                        }  
+                                            echo("<script>location.href = '/admin/newpost.php?msg=Image+size+too+big';</script>");
+                                            exit();
+                                        }
                                     }else{
-                                        echo("<script>location.href = '/admin/newpost.php?msg=Image+size+too+big';</script>");
+                                        echo("<script>location.href = '/admin/newpost.php?msg=Something+went+wrong';</script>");
                                         exit();
                                     }
                                 }else{
-                                    echo("<script>location.href = '/admin/newpost.php?msg=Something+went+wrong';</script>");
+                                    echo("<script>location.href = '/admin/newpost.php?msg=Wrong+file+type';</script>");
                                     exit();
                                 }
+
                             }else{
-                                echo("<script>location.href = '/admin/newpost.php?msg=Wrong+file+type';</script>");
-                                exit();
+                                #user dont want to update image
+                                $sql = "UPDATE `post` SET post_title='$post_title', post_content='$post_content', post_keyword='$post_keyword' WHERE post_id='$post_id'";
+                                if(mysqli_query($conn,$sql)){
+                                    echo("<script>location.href = '/admin/post.php?msg=Post+Updated';</script>");
+                                    exit();
+                                }else{
+                                    echo("<script>location.href = '/admin/newpost.php?msg=error';</script>");
+                                    exit(); 
+                                }
                             }
                         }
                     ?>
