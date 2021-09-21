ac87fdc41e9b7b73d7b51aae4ab9ca8f438fe2fd
Deepanshu Arya <deepanshu.da85@gmail.com>
2021-09-21 23:08:06 +0530

    added search feature

--- include/nav.php
+++ include/nav.php
@@ -44,8 +44,8 @@ include_once("connection.php");
         </ul>
         </li>
     </ul>
-    <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+    <form action="search.php"  class="d-flex">
+        <input name="s"class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
     </form>
     </div>
---
+++ search.php
@@ -0,0 +1,67 @@
+<?php 
+    include_once("include/connection.php");
+    include_once("include/functions.php");
+    if(!isset($_GET['s'])){
+        header("Location:index.php");
+        exit();
+    }else{
+        $search = mysqli_real_escape_string($conn,$_GET['s']);
+        $sql = "SELECT  * FROM `post` WHERE post_title LIKE '%$search%' OR post_content LIKE '%$search%' OR post_keyword LIKE '%$search%'"
+        ?>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>My Dynamic Site</title>
+            <link rel="stylesheet" href="style/bootstrap.min.css">
+            <link rel="stylesheet" href="style/style.css">
+            <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
+        </head>
+        <body>
+        
+            <?php include_once "include/nav.php";?>
+            <div class=container>
+            <h1>Showing result for: <?php echo $search; ?></h1>
+            <div class="row" data-masonry='{"percentPosition": true }'>
+            
+            <?php
+                $sql = "SELECT  * FROM `post` WHERE post_title LIKE '%$search%' OR post_content LIKE '%$search%' OR post_keyword LIKE '%$search%'";
+                $result = mysqli_query($conn, $sql);
+                while($row = mysqli_fetch_assoc($result)){
+                    $post_title = $row['post_title'];
+                    $post_content = $row['post_content'];
+                    $post_image = $row['post_image'];
+                    $post_id = $row['post_id'];
+                    $post_author = $row['post_author'];
+
+                    $sqlauthor = "SELECT * FROM `author` WHERE author_id='$post_author'";
+                    $resultauthor = mysqli_query($conn, $sqlauthor);
+                    while($rowauthor = mysqli_fetch_assoc($resultauthor)){
+                        $post_author_name = $rowauthor['author_name'];
+            ?>
+            <div class="col-sm-6 col-lg-4 mb-4">
+                <div class="card" style="width: 18rem;">
+                <img src="<?php echo $post_image; ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $post_title; ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $post_author_name; ?></h6>
+                    <p class="card-text"><?php echo substr(strip_tags($post_content),0,50).'...'; ?></p>
+                    <a href="post.php?id=<?php echo $post_id; ?>" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+            </div>
+            <?php }} ?>
+            
+            </div>
+            </div>
+
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/scroll.js"></script>
+        </body>
+        </html>
+        <?php
+    }
+?>
