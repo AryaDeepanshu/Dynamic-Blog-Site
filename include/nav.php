961986559e27bb82c7366eec2e74c72d80acf82b
Deepanshu Arya <deepanshu.da85@gmail.com>
2021-09-21 17:35:19 +0530

    category navigation bar

--- include/nav.php
+++ include/nav.php
@@ -1,3 +1,6 @@
+<?php 
+include_once("connection.php");
+?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="container-fluid">
     <a class="navbar-brand" href="#">My Dynamic Site</a>
@@ -7,20 +10,26 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
         <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
         </li>
         <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Categories
         </a>
+        <?php
+            $sql = 'SELECT * FROM `category`';
+            $result = mysqli_query($conn, $sql);
+        ?>
         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <?php 
+            while($row = mysqli_fetch_assoc($result)){
+                ?>
+            <li><a href="/category.php?id=<?php echo $row['category_id'];?>" class="dropdown-item"><?php echo $row['category_name'];?></a></li>
+            <?php }?>
+            
         </ul>
         </li>
     </ul>
